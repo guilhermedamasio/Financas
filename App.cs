e98d49e2571e8fb441cc3e6c689e57a1d876a2db
Guilherme <guilhermedamasiomendes@gmail.com>
2026-08-25 16:59:14 -0300

    Criada um classe APP que irá chamar tudo e resolver tudo, para facilitar a Program.

---
+++ App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financas
+{
+    public class App
+    {
+        public void Iniciar()
+        {
+            Menu meuMenu = new Menu();
+            meuMenu.ExibirOpcoes();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿using Financas;
 
-Menu meuMenu = new Menu();
-meuMenu.ExibirOpcoes();
+App meuApp = new App();
+meuApp.Iniciar();

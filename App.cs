6cd29cf991d13a076af6b8ad33fbf00aa8c3b6fc
Guilherme <guilhermedamasiomendes@gmail.com>
2026-08-26 14:05:16 -0300

    Basicamente, atualizado a aba App.cs, agora tem um loop de while/switch, de menu. Apenas isso.

--- App.cs
+++ App.cs
@@ -10,24 +10,31 @@ namespace Financas
         {
             Menu meuMenu = new Menu();
             meuMenu.ExibirOpcoes();
-            Painel respostaPainel = (Painel)Convert.ToInt32(Console.ReadLine());
-            switch(respostaPainel) 
+
+            Painel respostaPainel = (Painel)Convert.ToInt32(Console.ReadLine());  // 1ª leitura, ANTES do while
+
+            while (respostaPainel != Painel.Sair)  // testa a variável já existente
             {
-                case Painel.Receitas:
-                    Console.WriteLine("Você está no Painel de Receitas.");
-                    break;
-                case Painel.Despesa:
-                    Console.WriteLine("Você está no Painel de Despesas.");
-                    break;
-                case Painel.Investimento:
-                    Console.WriteLine("Você está no Painel de Investimentos.");
-                    break;
-                case Painel.Sair:
-                    Console.WriteLine("Saindo do aplicativo.");
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida.");
-                    break;
+                switch (respostaPainel)
+                {
+                    case Painel.Receitas:
+                        Console.WriteLine("Você está no Painel de Receitas.");
+                        break;
+                    case Painel.Despesa:
+                        Console.WriteLine("Você está no Painel de Despesas.");
+                        break;
+                    case Painel.Investimento:
+                        Console.WriteLine("Você está no Painel de Investimentos.");
+                        break;
+                    case Painel.Sair:
+                        Console.WriteLine("Saindo do aplicativo.");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+
+                respostaPainel = (Painel)Convert.ToInt32(Console.ReadLine());  // relê no FINAL do loop, atualiza a mesma variável
             }
         }
     }
